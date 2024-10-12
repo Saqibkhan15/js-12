ebf908964022c8400e3cb1e95ad66be2139fd30c
Saqibkhan15 <aqib12u@gmail.com>
2024-10-12 22:27:21 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+
+// var diceValue = Math.floor(Math.random() * 6) + 1;
+// document.write("The value of the dice roll is: " + diceValue);
+
+// var coinValue = Math.floor(Math.random() * 2) + 1;
+// var result;
+
+// if (coinValue === 1) {
+//     result = "Heads";
+// } else {
+//     result = "Tails";
+// }
+// document.write("The value of the coin toss is: " + result);
+
